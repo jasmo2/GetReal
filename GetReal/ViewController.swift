0ce369c839e84491edf7780a1a1e103c2620c4ad
Jaime Molina <jmolina@hugeinc.com>
2017-08-20 09:42:44 -0500

    Initial Commit

---
+++ GetReal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetReal
+//
+//  Created by Jaime Molina on 8/20/17.
+//  Copyright © 2017 Jaime Molina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
